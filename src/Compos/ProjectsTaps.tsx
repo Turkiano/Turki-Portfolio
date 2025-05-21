a7d477e2f126a55b0eae2dec80cb3e65bcd4c430
Turkiano <turki.saeed.ksa@gmail.com>
2025-05-21 10:37:41 +0300

    fixed the tab section for each project

--- src/Compos/ProjectsTaps.tsx
+++ src/Compos/ProjectsTaps.tsx
@@ -12,27 +12,75 @@ export function ProjectsTaps() {
   return (
     <Tabs defaultValue="Branding" className="w-full mx-auto mt-5">
       <TabsList className="flex justify-center space-x-4 w-auto mx-auto sticky top-0 z-50 bg-stone-200 dark:bg-gray-800 shadow border-b border-gray-300 dark:border-gray-700">
-        <TabsTrigger value="Branding"> {t("ProjectsTaps.tap01")}</TabsTrigger>
-        <TabsTrigger value="Research">
+        <TabsTrigger
+          value="Branding"
+          onClick={() => {
+            document
+              .getElementById("tab-content-Branding")
+              ?.scrollIntoView({ behavior: "smooth", block: "start" });
+          }}
+        >
+          {" "}
+          {t("ProjectsTaps.tap01")}
+        </TabsTrigger>
+        <TabsTrigger
+          value="Research"
+          onClick={() => {
+            document
+              .getElementById("tab-content-Research")
+              ?.scrollIntoView({ behavior: "smooth", block: "start" });
+          }}
+        >
           {t("ProjectsTaps.tap02")}
         </TabsTrigger>{" "}
-        <TabsTrigger value="content">{t("ProjectsTaps.tap03")}</TabsTrigger>
-        <TabsTrigger value="vfx">{t("ProjectsTaps.tap04")}</TabsTrigger>
-        <TabsTrigger value="Programing">{t("ProjectsTaps.tap05")}</TabsTrigger>
+        <TabsTrigger
+          value="content"
+          onClick={() => {
+            document
+              .getElementById("tab-content-content")
+              ?.scrollIntoView({ behavior: "smooth", block: "start" });
+          }}
+        >
+          {t("ProjectsTaps.tap03")}
+        </TabsTrigger>
+        <TabsTrigger
+          value="vfx"
+          onClick={() => {
+            document
+              .getElementById("tab-content-vfx")
+              ?.scrollIntoView({ behavior: "smooth", block: "start" });
+          }}
+        >
+          {t("ProjectsTaps.tap04")}
+        </TabsTrigger>
+        <TabsTrigger
+          value="Programing"
+          onClick={() => {
+            document
+              .getElementById("tab-content-Programing")
+              ?.scrollIntoView({ behavior: "smooth", block: "start" });
+          }}
+        >
+          {t("ProjectsTaps.tap05")}
+        </TabsTrigger>
       </TabsList>
-      <TabsContent value="Branding" className="w-full ">
+      <TabsContent
+        value="Branding"
+        className="w-full"
+        id="tab-content-Branding"
+      >
         <BrandingProjects />
       </TabsContent>
-      <TabsContent value="Programing">
+      <TabsContent value="Programing" id="tab-content-Programing">
         <ProgrammingProjects />
       </TabsContent>
-      <TabsContent value="Research">
+      <TabsContent value="Research" id="tab-content-Research">
         <ResearchProjects />
       </TabsContent>
-      <TabsContent value="content">
+      <TabsContent value="content" id="tab-content-content">
         <ContentProjects />
       </TabsContent>
-      <TabsContent value="vfx">
+      <TabsContent value="vfx" id="tab-content-vfx">
         <VfxProjects />
       </TabsContent>
     </Tabs>
